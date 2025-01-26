37f21d01c45a4f7667cf8d9a5c9306240655acee
Ashton Pate <arpate@amarillocollege.com>
2025-01-26 08:47:04 -0800

    updated commented codes

--- Wk2Ex1/Program.cs
+++ Wk2Ex1/Program.cs
@@ -12,22 +12,27 @@ double score = Convert.ToDouble(Console.ReadLine());
 //cauluates and outputs user score using an if else 
 if(score >= 89.5)
 {
+    //caluates A grade
     Console.WriteLine("The letter Grade for " + score + " is Grade A.");
 }
 else if (score  >= 79.5)
 {
+    //caluates B grade
     Console.WriteLine("The letter Grade for " + score + " is Grade B.");
 }
 else if (score >= 69.5)
 {
+    //caluates C grade
     Console.WriteLine("The letter Grade for " + score + " is Grade C.");
 }
 else if (score >= 59.5)
 {
+    //caluates D grade
     Console.WriteLine("The letter Grade for " + score + " is Grade D.");
 }
 else
 {
+    //caluates F grade
     Console.WriteLine("The letter Grade for " + score + " is Grade F.");
 }
 
