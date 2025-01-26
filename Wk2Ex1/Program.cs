d74a8ac3090c781514f70edce7be1ccc08bc3e36
Ashton Pate <arpate@amarillocollege.com>
2025-01-26 08:43:42 -0800

    Add project files.

---
+++ Wk2Ex1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System.Threading.Tasks.Sources;
+using static System.Formats.Asn1.AsnWriter;
+
+Console.WriteLine("Input score");
+
+
+double score = Convert.ToDouble(Console.ReadLine());
+
+if(score >= 89.5)
+{
+    Console.WriteLine("The letter Grade for " + score + " is Grade A.");
+}
+else if (score  >= 79.5)
+{
+    Console.WriteLine("The letter Grade for " + score + " is Grade B.");
+}
+else if (score >= 69.5)
+{
+    Console.WriteLine("The letter Grade for " + score + " is Grade C.");
+}
+else if (score >= 59.5)
+{
+    Console.WriteLine("The letter Grade for " + score + " is Grade D.");
+}
+else
+{
+    Console.WriteLine("The letter Grade for " + score + " is Grade F.");
+}
+
+
